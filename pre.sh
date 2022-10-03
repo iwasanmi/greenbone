990dca1f0e3cef4953a65bfd0c70235cc9d1f65e
iwasanmi <78426380+iwasanmi@users.noreply.github.com>
2022-10-03 15:14:07 +0000

    Update pre.sh

--- pre.sh
+++ pre.sh
@@ -1,3 +1,7 @@
 #/bin/bash
 
-apt-get install gcc cmake libglib2.0-dev libgnutls28-dev libpq-dev postgresql-server-dev-11 pkg-config libical-dev xsltproc libgpgme-dev
+curl -fsSL https://www.postgresql.org/media/keys/ACCC4CF8.asc|sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/postgresql.gpg
+echo "deb http://apt.postgresql.org/pub/repos/apt/ `lsb_release -cs`-pgdg main" |sudo tee  /etc/apt/sources.list.d/pgdg.list
+apt update
+
+apt-get -y install gcc cmake libglib2.0-dev libgnutls28-dev libpq-dev postgresql-server-dev-11 pkg-config libical-dev xsltproc libgpgme-dev
